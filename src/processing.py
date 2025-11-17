1f2cb93515c835df52a5a02e4e55fa4727d23c3c
DorkinDV <DorkinDV@gmail.com>
2025-11-18 00:15:26 +0800

    Homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -13,7 +13,7 @@ def filter_by_state(operations_list: List[Dict[str, any]], state: Optional[str] 
     return filtred_operations_list
 
 
-# # Пример использования функции:
+# # Пример функции:
 # data = [
 #     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
@@ -41,11 +41,11 @@ def sort_by_date(operations_list: List[Dict[str, any]], reverse: Optional[bool] 
 
 
 # Пример использования:
-data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+# data = [
+#     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 #
 # sorted_descending = sort_by_date(data)
 # print(sorted_descending)
@@ -54,4 +54,4 @@ data = [
 # print(sorted_ascending)
 
 
-print(filter_by_state(data, 'EXECUTED'))
+# print(filter_by_state(data, 'EXECUTED'))
